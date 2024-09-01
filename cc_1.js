c94bfd778415f44ce47d483e461bfd2e9f4a01cd
Melissa Montesinos <melissa.d.montesinos@gmail.com>
2024-09-01 17:25:41 -0400

    Task 1 - Employee Information

---
+++ cc_1.js
@@ -0,0 +1,11 @@
+// Task 1: Variables and Data Types
+
+let employeeName = "Tatiana Mieles";
+const employeeID = 77;
+var isActive = true;
+
+console.log(employeeName, typeof(employeeName));
+console.log(employeeID, typeof(employeeID));
+console.log(isActive, typeof(isActive));
+
+
