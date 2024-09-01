639e5d4b30ad167af89c1c22bf243123d28aba34
Melissa Montesinos <melissa.d.montesinos@gmail.com>
2024-09-01 18:43:05 -0400

    Task 4 - Employee Performance Evaluation

--- cc_1.js
+++ cc_1.js
@@ -33,12 +33,24 @@ console.log("Balance Deposit:",accountBalance);
 accountBalance -= 333;
 console.log("Balance Withdraw:", accountBalance);
 
-accountBalance *= 1.07
+accountBalance *= 1.07;
 console.log("Balance Interest:", accountBalance);
 
-accountBalance /= 1.1
-console.log("Balance Division:", accountBalance)
+accountBalance /= 1.1;
+console.log("Balance Division:", accountBalance);
 
-accountBalance %= 21
-console.log("Balance Remainder:", accountBalance)
+accountBalance %= 21;
+console.log("Balance Remainder:", accountBalance);
 
+
+// Task 4: Comparison Operators
+
+let employeeScore1= 87 ;
+let employeeScore2= 42 ;
+
+console.log(employeeScore1 > employeeScore2);
+console.log(employeeScore1 < employeeScore2);
+console.log(employeeScore1 >= employeeScore2);
+console.log(employeeScore1 <= employeeScore2);
+console.log(employeeScore1 === employeeScore2);
+console.log(employeeScore1 !== employeeScore2);
