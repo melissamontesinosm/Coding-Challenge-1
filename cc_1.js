4f755a29a216ba3a229531af05d53c2f53c8eaba
Melissa Montesinos <melissa.d.montesinos@gmail.com>
2024-09-01 18:26:36 -0400

    Task 3 - Financial Calculations

--- cc_1.js
+++ cc_1.js
@@ -20,3 +20,25 @@ const productDetails= {
 }
 
 console.log(products, productDetails);
+
+
+// Task 3: Assignment Operators
+
+let accountBalance = 1284.00;
+console.log ("Initial Balance:", accountBalance); 
+
+accountBalance +=230;
+console.log("Balance Deposit:",accountBalance);
+
+accountBalance -= 333;
+console.log("Balance Withdraw:", accountBalance);
+
+accountBalance *= 1.07
+console.log("Balance Interest:", accountBalance);
+
+accountBalance /= 1.1
+console.log("Balance Division:", accountBalance)
+
+accountBalance %= 21
+console.log("Balance Remainder:", accountBalance)
+
